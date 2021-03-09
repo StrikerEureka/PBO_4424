75cee39fca74b7055869e0e3d5aa4d5d36d9cb27
StrikerEureka <dinodamara11@gmail.com>
2021-03-10 01:15:12 +0700

    Tugas1

---
+++ src/Tugas1.java
@@ -0,0 +1,33 @@
+// Nama    : Dino D. Ramadhan
+// NIM     : A11.2019.11709
+// Kel.    : A11.4224
+// Project : Tugas Minggu 1
+
+public class Tugas1 {
+    public static void main(String[] args) {
+        soalA();
+        soalB();
+        soalC();
+        soalD();
+    }
+
+    private static void soalA() {
+        int hasil = -5 + 8 * 6;
+        System.out.println(hasil);
+    }
+
+    private static void soalB() {
+        int hasil = (55 + 9) % 9;
+        System.out.println(hasil);
+    }
+
+    private static void soalC() {
+        int hasil = 20 + -3 * 5 / 8;
+        System.out.println(hasil);
+    }
+
+    private static void soalD() {
+        int hasil = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(hasil);
+    }
+}
